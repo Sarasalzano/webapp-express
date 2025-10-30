4b6c977d00034a784c05f52ae977014126c851d2
Sara Salzano <sarasalzano95@gmail.com>
2025-10-30 15:32:05 +0100

    esegui index

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,3 +1,13 @@
 //import connection
 const connection = require("../data/movies-db");
 
+//index
+function index(req, res){
+    //prendi tutti i film
+    const sql= "SELECT * FROM movies";
+    connection.query(sql, (err, movies) => {
+    //gestisci errori
+    if (err) return res.status(500).json({error: "database query failed"});
+    res.json(movies);
+    })
+}
