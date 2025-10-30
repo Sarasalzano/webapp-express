58a501320e9c37d0558c7b891028eed3210353e6
Sara Salzano <sarasalzano95@gmail.com>
2025-10-30 14:43:13 +0100

    crea connection

--- data/movies-db.js
+++ data/movies-db.js
@@ -1,3 +1,18 @@
 //importo mysql
 const mysql = require("mysql2");
 
+//creo connessione al database
+const connection = mysql.createConnection({
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+//gestione errori
+connection.connect((err)=>{
+if (err) throw err;
+console.log("connected to mysql")
+});
+
+module.exports = connection;
