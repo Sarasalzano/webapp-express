de88c7033ff54872ebee07235be36652812a72df
Sara Salzano <sarasalzano95@gmail.com>
2025-11-04 15:16:03 +0100

    import middleware gestione errori e rotta inesistente

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const router = require("./routers/router");
+const errorHandler = require("./middlewares/errorHandler");
+const notFound = require("./middlewares/notFound");
+
+//import middleware gestione errore
+app.use(errorHandler);
+
+//import middleware rotta inesistente
+app.use(notFound);
 
 //avvia server
 app.listen(port, () => console.log(`App listening on port ${port}`));
