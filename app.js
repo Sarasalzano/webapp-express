c931faac5b3627f884da68c0f906ab40771b6a6e
Sara Salzano <sarasalzano95@gmail.com>
2025-11-04 15:14:33 +0100

    avvia server su porta d'ascolto

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const router = require("./routers/router");
+
+//avvia server
+app.listen(port, () => console.log(`App listening on port ${port}`));
