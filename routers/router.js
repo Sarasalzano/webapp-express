d87d6acaf910fdda28df931661b3d562fef946bf
Sara Salzano <sarasalzano95@gmail.com>
2025-11-05 14:46:37 +0100

    inserisci rotta store

--- routers/router.js
+++ routers/router.js
@@ -3,12 +3,15 @@ const express = require("express");
 //importa router 
 const router = express.Router();
 //import funzioni index e show
-const {index, show} = require("../controllers/controller");
+const {index, show, storeReview} = require("../controllers/controller");
 
 //index
 router.get("/", index);
 
 //show
 router.get("/:id", show);
+
+//store recensione
+router.get("/:id", storeReview);
 
 module.exports = router;
